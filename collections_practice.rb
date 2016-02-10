549c2f98972a101ab0811d2985d0a223635a7e8e
jbok4 <jbok4@aol.com>
2016-02-10 16:52:06 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
-# your code goes here
+def begins_with_r(array)
+  #array.include? { |e| e.match( /^r/ ) }
+end
+
+def contain_a(array)
+  array.select { |x| x.match( /a/ ) }
+end
+
+def first_wa(array)
+  array.find { |x| x.match( /wa/ ) }
+end
+
+def remove_non_strings(array)
+  array.each do |x|
+    if x.class == String
+      x.select
+    else
+      x.reject
+    end
+  end
+end
+
+def count_elements(array)
+  array.uniq{|d| d[:a]}
+end
