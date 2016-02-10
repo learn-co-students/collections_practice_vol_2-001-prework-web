2b944d1bbb71e13177346ba9507ddaae5444fc76
jbok4 <jbok4@aol.com>
2016-02-10 17:24:58 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,15 +11,18 @@ def first_wa(array)
 end
 
 def remove_non_strings(array)
-  array.each do |x|
-    if x.class == String
-      x.select
-    else
-      x.reject
+   #array.keep_if { |x| x.match( /^a..z/ ) }
+   array.each do |x|
+    if x = String.select!
+    else x.reject!
     end
   end
 end
 
 def count_elements(array)
   array.uniq{|d| d[:a]}
+end
+
+def merge_data(array)
+  array.join(", ")
 end
