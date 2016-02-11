7e676b4e5c6382f753dcd95fa0cc1b23adbc2a2b
jbok4 <jbok4@aol.com>
2016-02-10 23:57:58 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,8 @@
 def begins_with_r(array)
-  array.include? { |e| e.match( /^r/ ) }
+  #array.include? { |e| e.match( /^r/ ) }
+  array.all? do |x|
+  x[0] == "r"
+end
 end
 
 def contain_a(array)
@@ -11,19 +14,28 @@ def first_wa(array)
 end
 
 def remove_non_strings(array)
-   #array.keep_if { |x| x.match( /^a..z/ ) }
-   array.each do |x|
-    if x = String.select!
-    else x.reject!
-    end
-  end
+   array.keep_if { |x| x.is_string? }
+   #array.each do |x|
+    #if x.is_string? == !true 
+      #x.reject!
+    #end
+  #end
 end
 
 def count_elements(array)
-  array.uniq{|d| d[:a]}
+  #array.uniq{|d| d[:a]}
+  array.each_with_object(Hash.new(0)) { |word,counts| counts[word] += 1 }
 end
 
-def merge_data(array)
-  #array.join(", ")
-  #array.concat([array])
+def merge_data(keys, data)
+  #array.flatten
+  #array.reduce(:concat)
+end
+
+def find_cool(array)
+  array.find { |x| x.match /cool/ }
+end
+
+def organize_schools(array)
+
 end
