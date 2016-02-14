6452b22e2d439966ed147d860a0b80456f21a6a2
molotov2k <molotov2k@gmail.com>
2016-02-14 13:05:44 -0500

    5/8

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,34 +1,49 @@
 def begins_with_r(array)
-  if array.count {|x| x[0] == "r"} == array.length
+  if array.count {|elem| elem[0] == "r"} == array.length
     true
   else
     false
   end
 end
 
+
 def contain_a(array)
   arrayz = []
-  array.each do |x|
-    if x.include?("a")
-      arrayz << x
+  array.each do |elem|
+    if elem.include?("a")
+      arrayz << elem
     end
   end
   arrayz
 end
 
+
 def first_wa(array)
-  array.each do |x|
-    if x[0..1] == "wa"
-      return x
+  array.each do |elem|
+    if elem[0..1] == "wa"
+      return elem
       break
     end
   end
 end
 
+
 def remove_non_strings(array)
-  array.each do |x|
-    if x.is_a? String != true
-      delete_at(x)
-    end
+  array.delete_if {|elem| !elem.is_a? String}
+end
+
+
+def count_elements(array)
+  new_array = []
+  array.uniq.each do |elem|
+    elem[:count] = array.count(elem)
+    new_array << elem  
   end
+  new_array
+end  
+
+
+def merge_data(keys, data)
+  merged_data = data.merge(keys)
+  merged_data
 end
