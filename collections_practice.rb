0b7754fff02480301af76c7e69a3de1ff2bda909
molotov2k <molotov2k@gmail.com>
2016-02-13 19:31:45 -0500

    still in progress

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
-# your code goes here
+def begins_with_r(array)
+  if array.count {|x| x[0] == "r"} == array.length
+    true
+  else
+    false
+  end
+end
+
+def contain_a(array)
+  arrayz = []
+  array.each do |x|
+    if x.include?("a")
+      arrayz << x
+    end
+  end
+  arrayz
+end
+
+def first_wa(array)
+  array.each do |x|
+    if x[0..1] == "wa"
+      return x
+      break
+    end
+  end
+end
+
+def remove_non_strings(array)
+  array.each do |x|
+    if x.is_a? String != true
+      delete_at(x)
+    end
+  end
+end
