7486ed54347638f063a968347b3d188f0b1c3aec
Mayank Mishra <mayank.law@gmail.com>
2016-02-28 21:33:35 +0000

    nitrous-backup at Sun Feb 28 21:33:35 UTC 2016

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|word| word.start_with? "r"}
+end
+
+def contain_a(array)
+  words_with_a = []
+  array.each do |word|
+    if word.include? "a"
+      words_with_a << word
+    end
+  end
+  words_with_a
+end
+
+# .any, .include?, .find
+def first_wa(array)
+  array.find {|word| word.start_with "wa" }
+end
