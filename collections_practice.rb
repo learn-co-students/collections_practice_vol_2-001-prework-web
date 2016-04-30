b225d8e72d3ba8ead4fcbcf5b8dcf3d52d621680
supermancbs <clark.sacktor@gmail.com>
2016-04-30 13:59:00 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
 # your code goes here
+
+
+def begins_with_r(array)
+	count=0
+	array.each do |word|
+		if word.start_with?("r")
+			count+=1
+		end 
+	end 
+	
+	if count==array.length
+		return true 
+	end 
+		return false 
+end 
+
+
+def contain_a(array)
+	array.select do |word|
+		word.split("").include?("a")
+	end 
+end 
+
+def first_wa(array)
+	array.find do |word|
+		word[0..1]=="wa"
+	end 
+end 
+
+def remove_non_strings(array)
+	array.delete_if do |element|
+		element.class!=String
+	end 
+	
+end 
+
+def count_elements(array)
+	result =[]
+	array.each do |word|
+		#if result.has_value?(word)==false 
+			result << {:name => word, :count => array.count(word)}
+		#end 
+	end 
+	result 
+end 
+
+
+def merge_data(keys, data)
+	keys.merge(data)
+
+end 
