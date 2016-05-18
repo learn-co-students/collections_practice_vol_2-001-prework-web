5692842a76ce23a15afec072e03501c455d98e52
Kellye Greene <kellye@gust.com>
2016-05-18 14:43:55 -0400

    Getting this challenge started

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
-# your code goes here
+def begins_with_r(tools)
+  tools.all? { |word| word.start_with?("r") == true }
+end
+
+def contain_a(elements)
+  elements_with_a = []
+  elements_with_a << elements.select {|element| element.include?("a") }
+  elements_with_a.flatten
+end
+
+def first_wa(elements)
+  elements.find{ |element| element.start_with?("wa") == true }
+end
+
+def remove_non_strings(array)
+end
+
+def count_elements(array)
+end
+
+def merge_data(data)
+end
+
+def find_cool(temperature)
+end
+
+def organize_schools(schools)
+end
