e415a1e564371928f08fd75259a81a2e0fa93c01
Salome Braganza <salomekbg@gmail.com>
2016-06-17 17:23:47 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,12 +34,11 @@ end
 def organize_schools(schools)
   new_hash = {}
   city_array = []
-  schools.each {|school, place| city_array = place.values}
-  return city_array
+  schools.each {|school, place| place.each {|location, city| city_array << city if city_array.include?(city) == false}}
 
   city_array.map.with_index {|city, i|
     school_array = []
     schools.map {|school, place| school_array << school if place.value?(city_array[i])}
-    new_hash[city] = school_array}
-#  new_hash
+  new_hash[city] = school_array}
+  new_hash
 end
