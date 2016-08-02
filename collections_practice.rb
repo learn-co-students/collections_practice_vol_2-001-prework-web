e5727115153fd3aba09823ad43b25cbde85d3147
Jason Arnold <jason.arnold.00@gmail.com>
2016-08-02 01:39:35 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
 # your code goes here
+def begins_with_r(arr)
+    arr.each do |word|
+    	if word[0] != 'r'
+    		return false
+    	end
+    end
+    return true
+end
+
+def contain_a(arr)
+    arr.select do |word|
+      word.include?('a')
+    end
+end
