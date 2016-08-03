ec8f3f8070f4428dbfe04e587d9198e02ac18bcb
Jason Arnold <jason.arnold.00@gmail.com>
2016-08-03 02:43:41 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,3 +13,14 @@ def contain_a(arr)
       word.include?('a')
     end
 end
+
+def first_wa(arr)
+	new_arr = []
+  	arr.each do |word|
+  		word = word.to_s
+  		new_arr.push(word)
+  	end
+  	new_arr.find do |word|
+  		word.include?('wa')
+  	end
+end
