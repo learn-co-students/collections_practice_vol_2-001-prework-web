a04d2d9a4eaa30a4264f4a87691927ff7fece335
dimacodes <dimitriserov@gmail.com>
2016-09-30 13:33:12 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,17 +1,17 @@
 def begins_with_r(array)
-  array.all? { |element| element[0] == "r" }
+  array.all? {|element| element[0] == "r"}
 end
 
 def contain_a(array)
-  array.select { |element| element.include? "a" }
+  array.select {|element| element.include? "a"}
 end
 
 def first_wa(array)
-  array.find { |element| element.to_s.include? "wa" }
+  array.find {|element| element.to_s.include? "wa"}
 end
 
 def remove_non_strings(array)
-  array.delete_if { |element| element.class != String }
+  array.delete_if {|element| element.class != String}
 end
 
 def count_elements(array)
